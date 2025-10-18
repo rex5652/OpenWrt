d265be310eebf1c473eaa940cf1ce2f6e7104a30
Rex Chen <rex5652@gmail.com>
2025-10-18 13:02:09 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -122,4 +122,4 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 ./scripts/feeds install -a
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
