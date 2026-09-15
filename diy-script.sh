a200c9d727aea79754f93a071262e19e1336479d
Rex Chen <rex5652@gmail.com>
2026-09-16 00:38:08 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -15,6 +15,7 @@ rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -fr feeds/packages/net/helloworld
+rm -rf feeds/helloworld/gn package/feeds/helloworld/gn
 ## rm -rf feeds/luci/themes/luci-theme-argon
 ## rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-mosdns
